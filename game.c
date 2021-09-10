250cfb743f841ff0e628b5ec57235250e52d759a
yonoyo <90102917+yonoyo@users.noreply.github.com>
2021-09-10 19:22:19 +0800

    Add files via upload

---
+++ game.c
@@ -0,0 +1,40 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+void menu(){
+
+	printf("1.play      0.exit\n");
+
+
+}
+//void play(){
+//	int ret = 0;
+//	
+//	ret = srand()%100+1;
+//	printf("%s", ret);
+//}
+
+int main(){
+	menu();
+	srand(unsigned int)time(NULL);
+	/*int input = 0;
+	do{
+		printf("«Î—°‘Ò£∫\n");
+		scanf("%d", &input);
+		switch (input){
+		case 1:
+			play();
+			break;
+		case 0:
+			
+			break;
+		default :
+			break;
+		}
+	} while (input);*/
+	system("pause");
+	return 0;
+	}
+	
+
